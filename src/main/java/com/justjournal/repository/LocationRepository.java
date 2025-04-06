b982d7de6c571804abeeecf2dc90cbcaf9fdb7ac
Lucas Holt <luke@foolishgames.com>
2025-04-05 23:26:52 -0400

    override

--- src/main/java/com/justjournal/repository/LocationRepository.java
+++ src/main/java/com/justjournal/repository/LocationRepository.java
@@ -28,11 +28,14 @@ package com.justjournal.repository;
 
 import com.justjournal.model.Location;
 import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Integer> {
 
-  List<Location> findAll();
+  @Override
+  @NotNull List<Location> findAll();
 }
