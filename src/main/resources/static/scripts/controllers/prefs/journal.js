a033e54ff12dff9bd0c4d8c505ab08380ff3cc01
Lucas Holt <luke@foolishgames.com>
2025-04-14 00:49:03 -0400

    cleanup

--- src/main/resources/static/scripts/controllers/prefs/journal.js
+++ src/main/resources/static/scripts/controllers/prefs/journal.js
@@ -39,10 +39,13 @@ angular.module('wwwApp').controller('PrefsJournalCtrl', ['$scope', 'LoginService
                             $scope.ErrorMessage = response.data.error;
                         }
                         else if (typeof(response.data.ModelState) !== 'undefined') {
-                            $scope.ErrorMessage = response.data.Message + ' (' + response.status + ') ' + angular.toJson(response.data.ModelState);
+                            $scope.ErrorMessage = response.data.Message + ' (' + response.status + ') ' +
+                                angular.toJson(response.data.ModelState);
                         }
                         else if (typeof(response.data.ExceptionMessage) !== 'undefined') {
-                            $scope.ErrorMessage = response.data.Message + ' (' + response.status + ') ' + response.data.ExceptionMessage + ' ' + response.data.ExceptionType + ' ' + response.data.StackTrace;
+                            $scope.ErrorMessage = response.data.Message + ' (' + response.status + ') ' +
+                                response.data.ExceptionMessage + ' ' + response.data.ExceptionType + ' ' +
+                                response.data.StackTrace;
                         }
                         else {
                             $scope.ErrorMessage = 'Unknown error occurred. Response was ' + angular.toJson(response);
